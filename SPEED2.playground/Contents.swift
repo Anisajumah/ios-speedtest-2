529c7edbf0b829a3a95937c8306b300ca5a82e1b
Anisajumah <70814404+Anisajumah@users.noreply.github.com>
2020-09-16 17:26:50 +0300

    done

--- SPEED2.playground/Contents.swift
+++ SPEED2.playground/Contents.swift
@@ -8,7 +8,20 @@
 
 */
 var numbers = [1,2,3,4,5,6,7,8,9,10]
-
+var evennumbers = [2,4,6,8,10]
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+print(numbers[6])
+print(numbers[7])
+print(numbers[8])
+print(numbers[9])
+for i in 0...100 {
+    print("evennumbers")
+}
 
 
 /*:
